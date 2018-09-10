587e63e1b2020f8e6bc50bf538539b1939d8b0d4
Glutamatt <glutamatt@live.fr>
2018-09-10 18:52:47 +0200

    Theme

--- server.go
+++ server.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello you 2, %q", html.EscapeString(r.URL.Path))
-	})
+	servicePort := os.Getenv("SERVER_LISTENING_PORT")
+	if servicePort == "" {
+		servicePort = "443"
+	}
 
-	log.Fatal(http.ListenAndServe(":443", nil))
+	css, err := ioutil.ReadFile("assets/theme.css")
+	if err != nil {
+		panic(err)
+	}
+	page, err := ioutil.ReadFile("assets/page.html")
+	if err != nil {
+		panic(err)
+	}
+
+	content := "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, content) })
+
+	log.Fatal(http.ListenAndServe(":"+servicePort, nil))
 }
