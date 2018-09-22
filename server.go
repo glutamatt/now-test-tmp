fe19110e57d1c5cdf80c19f9c232ced3c7b41efd
Glutamatt <glutamatt@live.fr>
2018-09-22 18:36:20 +0200

    background

--- server.go
+++ server.go
@@ -26,18 +26,25 @@ func main() {
 
 	content := "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
 
-	http.HandleFunc("/sound.mp3", func(w http.ResponseWriter, r *http.Request) {
-		file, err := http.Get("https://raw.githubusercontent.com/glutamatt/now-test-tmp/7a46bb9a6e24b97058a17949cb2ad5cfca3dbf68/sound.mp3")
-		if err != nil {
-			fmt.Fprint(w, err.Error())
-			return
-		}
-		defer file.Body.Close()
-		_, err = io.Copy(w, file.Body)
-		if err != nil {
-			fmt.Fprint(w, err.Error())
-		}
-	})
+	onlineAssets := map[string]string{
+		"/sound.mp3":      "https://raw.githubusercontent.com/glutamatt/now-test-tmp/7a46bb9a6e24b97058a17949cb2ad5cfca3dbf68/sound.mp3",
+		"/background.jpg": "https://raw.githubusercontent.com/glutamatt/now-test-tmp/7a46bb9a6e24b97058a17949cb2ad5cfca3dbf68/sound.mp3",
+	}
+
+	for path, dest := range onlineAssets {
+		http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			file, err := http.Get(dest)
+			if err != nil {
+				fmt.Fprint(w, err.Error())
+				return
+			}
+			defer file.Body.Close()
+			_, err = io.Copy(w, file.Body)
+			if err != nil {
+				fmt.Fprint(w, err.Error())
+			}
+		})
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, content) })
 
