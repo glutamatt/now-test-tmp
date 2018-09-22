7a46bb9a6e24b97058a17949cb2ad5cfca3dbf68
Glutamatt <glutamatt@live.fr>
2018-09-22 18:11:43 +0200

    Test

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -25,7 +26,20 @@ func main() {
 
 	content := "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
 
+	http.HandleFunc("/sound.mp3", func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open("sound.mp3")
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err)
+		}
+		_, err = io.Copy(w, file)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err)
+		}
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, content) })
 
-	log.Fatal(http.ListenAndServe(":"+servicePort, nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+servicePort, nil))
 }
