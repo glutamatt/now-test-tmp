0ca196616b5a09e9c2287d2c7323dfab8fa15e1f
Glutamatt <glutamatt@live.fr>
2018-09-22 18:28:28 +0200

    Works

--- server.go
+++ server.go
@@ -27,19 +27,19 @@ func main() {
 	content := "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
 
 	http.HandleFunc("/sound.mp3", func(w http.ResponseWriter, r *http.Request) {
-		file, err := os.Open("sound.mp3")
+		file, err := http.Get("https://raw.githubusercontent.com/glutamatt/now-test-tmp/7a46bb9a6e24b97058a17949cb2ad5cfca3dbf68/sound.mp3")
 		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprint(w, err)
+			fmt.Fprint(w, err.Error())
+			return
 		}
-		_, err = io.Copy(w, file)
+		defer file.Body.Close()
+		_, err = io.Copy(w, file.Body)
 		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprint(w, err)
+			fmt.Fprint(w, err.Error())
 		}
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, content) })
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:"+servicePort, nil))
+	log.Fatal(http.ListenAndServe(":"+servicePort, nil))
 }
