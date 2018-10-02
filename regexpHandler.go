5dbe0a36a61032511b17870be185f808102b7f71
Glutamatt <glutamatt@live.fr>
2018-10-02 15:02:59 +0200

    Assets with redirect http

---
+++ regexpHandler.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type route struct {
+	pattern *regexp.Regexp
+	handler http.Handler
+}
+
+type RegexpHandler struct {
+	routes []*route
+}
+
+func (h *RegexpHandler) Handler(pattern string, handler http.Handler) {
+	h.routes = append(h.routes, &route{regexp.MustCompile(pattern), handler})
+}
+
+func (h *RegexpHandler) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	h.routes = append(h.routes, &route{regexp.MustCompile(pattern), http.HandlerFunc(handler)})
+}
+
+func (h *RegexpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range h.routes {
+		if route.pattern.MatchString(r.URL.Path) {
+			route.handler.ServeHTTP(w, r)
+			return
+		}
+	}
+	http.NotFound(w, r)
+}
--- server.go
+++ server.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,7 +14,23 @@ func main() {
 	if servicePort == "" {
 		servicePort = "443"
 	}
+	assetsBase := "https://raw.githubusercontent.com/glutamatt/now-test-tmp/fe19110e57d1c5cdf80c19f9c232ced3c7b41efd/"
 
+	home := homeContent()
+	handler := &RegexpHandler{}
+
+	handler.HandleFunc("/assets", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, assetsBase+strings.TrimPrefix(r.RequestURI, "/assets/"), http.StatusPermanentRedirect)
+	})
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, home)
+	})
+
+	log.Println("Server listening on : http://0.0.0.0:" + servicePort)
+	log.Fatal(http.ListenAndServe(":"+servicePort, handler))
+}
+
+func homeContent() string {
 	css, err := ioutil.ReadFile("assets/theme.css")
 	if err != nil {
 		panic(err)
@@ -25,33 +40,5 @@ func main() {
 		panic(err)
 	}
 
-	content := "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
-
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	onlineAssets := map[string]string{
-		"/sound.mp3":      "https://raw.githubusercontent.com/glutamatt/now-test-tmp/fe19110e57d1c5cdf80c19f9c232ced3c7b41efd/sound.mp3",
-		"/background.jpg": "https://raw.githubusercontent.com/glutamatt/now-test-tmp/fe19110e57d1c5cdf80c19f9c232ced3c7b41efd/background.jpg",
-	}
-
-	for path, dest := range onlineAssets {
-		func(path, dest string) {
-			http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-				file, err := http.Get(dest)
-				if err != nil {
-					fmt.Fprint(w, err.Error())
-					return
-				}
-				defer file.Body.Close()
-				_, err = io.Copy(w, file.Body)
-				if err != nil {
-					fmt.Fprint(w, err.Error())
-				}
-			})
-		}(path, dest)
-
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, content) })
-
-	log.Fatal(http.ListenAndServe(":"+servicePort, nil))
+	return "<style type=\"text/css\">" + string(css) + "</style>" + string(page)
 }
